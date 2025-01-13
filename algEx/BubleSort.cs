98f52d5d771f9248c5841cbf8aecf727efccce92
ifysglbiydgfifs <igbsh43hsdh@gmail.com>
2025-01-14 01:00:27 +0500

    BubleSort

---
+++ algEx/BubleSort.cs
@@ -0,0 +1,20 @@
+namespace algEx;
+
+public class BubleSort
+{
+    public static void Run(int[] array)
+    {            
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = 0; j < array.Length - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
---
+++ algEx/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using algEx;
+
+var testArray = new int[10]{3,6,12,53,2,34,7,23,19,8};
+
+BubleSort.Run(testArray);
+
+foreach (var VARIABLE in testArray)
+{
+    Console.WriteLine(VARIABLE);
+}
